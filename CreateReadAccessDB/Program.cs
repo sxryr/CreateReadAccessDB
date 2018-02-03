df5c553ce891c639a0bf67fab1168fec477caa6b
Shaharyar <bsninty@gmail.com>
2018-02-03 18:47:41 +0500

    initial commit

--- CreateReadAccessDB/Program.cs
+++ CreateReadAccessDB/Program.cs
@@ -10,6 +10,27 @@ namespace CreateReadAccessDB
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("PRESS 1 TO VIEW ALL ENTRIES\nPRESS 2 ADD ENTRY\n" +
+                "PRESS 3 TO EXIT\n");
+            
+            while (true)
+            {
+                Console.Write(">");
+                string Choice = Console.ReadLine();
+                if (Choice == "1")
+                {
+                    methods.GetEntries();
+                }
+                else if (Choice == "2")
+                {
+                    methods.InsertEntry();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
---
+++ CreateReadAccessDB/methods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateReadAccessDB
+{
+    class methods
+    {
+        //GLOBAL VARIABLES
+        static OleDbConnection connectionObject;
+        static OleDbCommand commandObject;
+        static OleDbDataReader readerObject;
+
+        public static void GetEntries()
+        {
+            string Entries = "";
+            connectionObject = new OleDbConnection();
+            connectionObject.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                    "Data Source= database/example.accdb";
+            commandObject = new OleDbCommand();
+            commandObject.Connection = connectionObject;
+            commandObject.CommandText = "SELECT * FROM Table1";
+            connectionObject.Open();
+            readerObject = commandObject.ExecuteReader();
+            while (readerObject.Read())
+            {
+                Entries += readerObject[0] + "-" + readerObject[1] + "\n";
+            }
+            Console.WriteLine(Entries + "\n");
+            connectionObject.Close();
+        }
+
+        public static void InsertEntry()
+        {
+            Console.Write("Entry:");
+            string Entry = Console.ReadLine();
+            connectionObject = new OleDbConnection();
+            connectionObject.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                    "Data Source=database/example.accdb";
+            commandObject = new OleDbCommand();
+            commandObject.Connection = connectionObject;
+            commandObject.CommandText = "INSERT INTO Table1 (scratch) VALUES ('" + Entry + "')";
+            connectionObject.Open();
+            commandObject.ExecuteNonQuery();
+            connectionObject.Close();
+            Console.WriteLine("Success!\n");
+        }
+    }
+}
